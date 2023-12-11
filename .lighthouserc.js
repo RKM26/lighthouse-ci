79916dcecba4bfef6a6e1e241fa27c9f5b7999f9
rishabh26666 <rishabhkumarmayank@gmail.com>
2023-12-11 16:28:29 +0530

    lighthouse asserts added

--- .lighthouserc.js
+++ .lighthouserc.js
@@ -4,8 +4,24 @@ module.exports = {
         // collect options here
       },
       assert: {
-        // assert options here
-      },
+        "preset": "lighthouse:recommended", // Change this to "lighthouse:recommended" when we move to a PWA in the future
+        assertions: {
+            "maskable-icon": "off",
+            "service-worker": "off",
+            "tap-targets": "off",
+            "apple-touch-icon": "off",
+            "first-contentful-paint": { minScore: 0.9 },
+            "first-cpu-idle": { minScore: 0.9 },
+            "first-cpu-idle": { minScore: 0.9 },
+            "interactive": { minScore: 0.9 },
+            "last-contentful-paint": { minScore: 0.9 },
+            "max-potential-fid": { minScore: 0.9 },
+            "render-blocking-resources": { minScore: 0.9 },
+            "speed-index": { minScore: 0.9 },
+            'categories:performance': ['error', { minScore: 0.9 }],
+            'categories:accessibility': ['warn', { minScore: 0.9 }],
+        }
+    },
       upload: {
         // upload options here
       },
